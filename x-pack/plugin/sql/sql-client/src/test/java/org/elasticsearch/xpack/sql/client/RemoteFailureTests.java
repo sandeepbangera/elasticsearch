bc4a881896f437a88bfb157aeb032754b0836ccc
Michael Gianatassio <mgianata@dhcp-10-39-205-71.vpn.oracle.com>
2020-07-13 13:33:33 -0400

    fix copyright notices

--- x-pack/plugin/sql/sql-client/src/test/java/org/elasticsearch/xpack/sql/client/RemoteFailureTests.java
+++ x-pack/plugin/sql/sql-client/src/test/java/org/elasticsearch/xpack/sql/client/RemoteFailureTests.java
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Copyright (C) 2020, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package org.elasticsearch.xpack.sql.client;
